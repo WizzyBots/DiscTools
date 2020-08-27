263c3810fa865968f8ade2886d3756cb5550592e
TEEN_BOOM <ojasscoding@gmail.com>
2020-08-27 11:42:01 +0530

    bump version

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ def test_suite():
     return unittest.TestLoader().loadTestsFromNames(["test_bot", "test_cmd", "test_context"])
 
 if __name__ == "__main__":
+    # Running this file as main tests local code not the installed code
     import sys
     import os
     sys.path.insert(0, os.path.abspath("."))
